2c31b734096f790ed6ded9f487b1c9e675d145e7
zs-andy <andy070119@outlook.com>
2026-07-13 23:12:08 +0800

    Prerender localized marketing pages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
+import { getMessages, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import "../globals.css";
@@ -22,6 +22,10 @@ type Props = {
 };
 
 const metadataBase = new URL("https://www.tomeet.chat");
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export async function generateMetadata({ params }: Pick<Props, "params">): Promise<Metadata> {
   const { locale } = await params;
@@ -92,6 +96,7 @@ export default async function LocaleLayout({ children, params }: Props) {
     notFound();
   }
 
+  setRequestLocale(locale);
   const messages = await getMessages();
 
   return (
